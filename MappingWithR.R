d3c74cdc4b9d022154d421d1d5637a76698fd434
Rebecca <rebeccahirsch@Rebeccas-MacBook-Air.local>
2020-06-17 18:22:21 -0700

    put graphs and charts into folder

--- MappingWithR.R
+++ MappingWithR.R
@@ -1,18 +1,27 @@
 #Making maps of trends in Midtown Sacramento Apartments
 #using ggplot2, ggmaps, 
 #Written by Rebecca Hirsch 6/5/2020 Spafford & Lincoln, Inc.
+#Sources: https://datascienceplus.com/visualising-thefts-using-heatmaps-in-ggplot2/
 
-MTData <- read.csv("~/Desktop/S&L/Coding/Midtown_Apartments/MTData1.csv")
-summary(MTData)
 
-locations <- as_tibble(MTData)
-locations_sf <- st_as_sf(locations, coords = c("Longitude", "Latitude"), crs = 4326)
-mapview(locations_sf) #121.477, 38.571 zoom 15
+MTData2 <- read.csv("~/Desktop/S&L/Coding/Midtown_Apartments/MTData2.csv")
+summary(MTData2)
 
-map <- get_googlemap(center = c(-121.481, 38.573), zoom = 15,
-                     style = "element:labels|visibility:off&style=feature:administrative|visibility:off")
-ggmap(map) +
-  geom_point(data = locations, aes(x = lon, y = lat, size = X..Sqft, color = X..Sqft), shape = 20, alpha = 0.8) +
-  scale_size_continuous(range=c(1,14)) +
-  scale_color_viridis(option = "A" , trans="log")
+library(ggmap)
 
+midtown <- get_map(c(-121.481, 38.573), zoom = 15)
+ggmap(midtown)
+
+aptLocations <- as.data.frame(table(MTData2$Longitude, MTData2$Latitude, MTData2$X..Sqft))
+names(aptLocations) <- c("long", "lat", "cost", "Frequency")
+aptLocations$long <- as.numeric(as.character(aptLocations$long))
+aptLocations$lat <- as.numeric(as.character(aptLocations$lat))
+aptLocations$cost <- as.numeric(as.character(aptLocations$cost))
+aptLocations <- subset(aptLocations, Frequency > 0)
+
+png("CostPerFootMapped.png", width = 650, height = 750)
+ggmap(midtown) + 
+  geom_point(data = aptLocations, aes(x = long, y = lat, color = cost, size = cost, alpha = 0.5)) +
+    scale_color_continuous(low = "green", high = "red", breaks = c(1,1.5,2,2.5,3,3.5,4,4.5)) +
+    scale_size(breaks = c(1,1.5,2,2.5,3,3.5,4,4.5), range = c(1,15))
+dev.off()
